148a4828d64be134e2712bd8efb9c5833d0f6ae4
Luka Maisuradze <codeverselm@gmail.com>
2025-09-23 20:48:40 +0400

    adds share icon to sharelink

--- src/app/modules/shareLinks.tsx
+++ src/app/modules/shareLinks.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { shareLinksData } from "../../constants/shareLinks";
+import { FaRegShareSquare } from "react-icons/fa";
 
 const ShareLinks = ({
   orientation,
@@ -12,32 +13,43 @@ const ShareLinks = ({
   const flexDirection = orientation === "column" ? "flex-col" : "flex-row";
 
   return (
-    <ul className={`flex ${flexDirection} items-center justify-center gap-2`}>
-      {shareLinksData.map((link) => (
-        <li>
-          <Link
-            href={link.id}
-            className={` flex items-center justify-center rounded-full ${
-              theme === "light"
-                ? "rounded-full w-[37px] h-[37px]"
-                : "rounded-xs w-[32px] h-[32px]"
-            }`}
-            style={
-              theme === "light"
-                ? { backgroundColor: "#FFFFFF" }
-                : { backgroundColor: link.color }
-            }
-          >
-            <link.icon
-              className="w-1/2 h-1/2"
+    <div className={`flex ${flexDirection} items-center justify-between gap-4`}>
+      <div
+        className={`w-max flex ${flexDirection} items-center gap-2 text-[#333333]`}
+      >
+        <FaRegShareSquare className="w-[22px] h-[22px]" />
+        <span className="text-xs font-bold">SHARE</span>
+      </div>
+
+      <ul className={`flex ${flexDirection} items-center justify-center gap-2`}>
+        {shareLinksData.map((link) => (
+          <li>
+            <Link
+              href={link.id}
+              className={` flex items-center justify-center rounded-full ${
+                theme === "light"
+                  ? "rounded-full w-[37px] h-[37px]"
+                  : "rounded-xs w-[32px] h-[32px]"
+              }`}
               style={
-                theme === "light" ? { color: link.color } : { color: "#FFFFFF" }
+                theme === "light"
+                  ? { backgroundColor: "#FFFFFF" }
+                  : { backgroundColor: link.color }
               }
-            />
-          </Link>
-        </li>
-      ))}
-    </ul>
+            >
+              <link.icon
+                className="w-1/2 h-1/2"
+                style={
+                  theme === "light"
+                    ? { color: link.color }
+                    : { color: "#FFFFFF" }
+                }
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Business from "./modules/business";
 import FollowUs from "./modules/followUs";
 import Gallery from "./modules/gallery";
+import ShareLinks from "./modules/shareLinks";
 
 export default function Home() {
   return (
     <>
-      <div className="w-full mx-auto">
-        <FollowUs breakpoint={300} />
+      <div className="w-full mx-auto h-[40000px]">
+        <ShareLinks orientation="row" theme="dark" />
       </div>
 
       <Gallery />
