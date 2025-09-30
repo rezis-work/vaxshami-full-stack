1215a9040dc7bcd8ddae9628817da9acba1c2653
MakoDoli <makodolidze@hotmail.com>
2025-09-30 13:26:25 +0400

    add blogcards and blogcontainer

---
+++ src/components/shared/wrapper.tsx
@@ -0,0 +1,14 @@
+interface props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Wrapper({ children, className }: props) {
+  return (
+    <div
+      className={`px-4 sm:max-w-xl sm:px-4 md:max-w-3xl md:px-8 lg:px-10 lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl 2xl:px-0  mx-auto ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
