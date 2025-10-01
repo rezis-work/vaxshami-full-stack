6eb3da5194c081ca55f558075aca0761d3fb5b54
Luka Maisuradze <codeverselm@gmail.com>
2025-10-01 11:45:48 +0400

    renames hero component to topStory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import BreakingNews from "@/features/breakingNews/ui/views/breakingNews-view";
 import Business from "@/features/business/ui/views/business-view";
 import DontMiss from "@/features/dontMiss/ui/views/dontMiss-view";
 import Gallery from "@/features/gallery/ui/views/gallery-view";
-import Hero from "@/features/hero/ui/views/hero-view";
+import TopStory from "@/features/topStory/ui/views/topStory-view";
 
 export default function Home() {
   return (
     <>
       <BreakingNews />
-      <Hero />
+      <TopStory />
       <Gallery />
       <Business />
       <DontMiss className="purpleLinearBackground" />
--- src/features/topStory/ui/views/topStory-view.tsx
+++ src/features/topStory/ui/views/topStory-view.tsx
@@ -3,7 +3,7 @@ import HeroContent from "../components/heroContent";
 import Wrapper from "@/components/shared/wrapper";
 import HeroCards from "../components/heroCards";
 
-const Hero = () => {
+const TopStory = () => {
   return (
     <section className="relative w-full flex flex-col justify-between py-9 rounded-sm bg-[url('/slider-3.png')] bg-cover bg-center aspect-[4/2.8] lg:aspect-[4/2] xl:aspect-[4/1.5] transition-all duration-300">
       <div className="absolute inset-0 bg-black/30 rounded-sm" />
@@ -17,4 +17,4 @@ const Hero = () => {
   );
 };
 
-export default Hero;
+export default TopStory;
