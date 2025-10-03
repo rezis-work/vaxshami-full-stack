de72716a6fafe5ac021a172f90896c3b6fd88634
MakoDoli <makodolidze@hotmail.com>
2025-10-03 21:51:47 +0400

    fix layout

--- src/features/defaultPost/ui/components/defaultSidebar.tsx
+++ src/features/defaultPost/ui/components/defaultSidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DefaultSidebar() {
   return (
-    <div className="mb-15 px-[15px] lg:flex-shrink-0 lg:flex-grow-0 lg:basis-1/3 lg:max-w-1/3 lg:relative bg-blue-300 ">
+    <div className="mb-15 px-[15px] lg:flex-shrink-0 lg:flex-grow-0 lg:basis-1/3 lg:max-w-1/3 lg:relative  ">
       defaultSidebar
     </div>
   );
--- src/features/defaultPost/ui/views/defaultPost-view.tsx
+++ src/features/defaultPost/ui/views/defaultPost-view.tsx
@@ -5,7 +5,7 @@ import DefaultSidebar from "../components/defaultSidebar";
 export default function DefaultPostView() {
   return (
     // container
-    <div className="m-auto sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1280px] px-[15px]">
+    <div className="m-auto mt-[30px] sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1280px] px-[15px]">
       {/* row */}
       <div className="flex flex-wrap justify-center mx-[-15px]">
         <DefaultPost post={blogCardsData[5]} />
