a86072e11133cfd81d8584a1e817c5b8bd79f148
luka <codeverselm@gmail.com>
2025-10-08 21:45:39 +0400

    fixes gallery title

--- src/features/gallery/ui/views/gallery-view.tsx
+++ src/features/gallery/ui/views/gallery-view.tsx
@@ -9,7 +9,7 @@ const Gallery = () => {
       <Wrapper className="relative grid grid-cols-1 lg:grid-cols-7 xl:grid-cols-3 gap-6 pt-30 pb-16 mx-auto ">
         <SectionTitle
           title="Gallery"
-          styles="absolute left-4 md:left-8 lg:left-10 2xl:left-0 top-18 items-end"
+          styles="absolute left-4 sm:left-0 md:left-0 top-18 items-end"
         />
 
         <MainPost />
