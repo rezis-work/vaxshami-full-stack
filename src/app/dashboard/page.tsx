ff2bae535eae9b95e79585b65ba4c5402d413a01
Toko122 <toko.migine@gmail.com>
2025-10-15 18:21:58 +0400

    Admin-sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
+import AdminSidebar from '../../features/dashboard/components/AdminSidebar'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      hello world
+    <div className='min-h-screen w-full bg-[#ECF0FA]'>
+      <AdminSidebar />
     </div>
   )
 }
