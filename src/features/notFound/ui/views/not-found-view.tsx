83890a5d4e06c917772ecd0d38d2667eb05fe9d8
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-10-24 23:24:56 +0400

    hydratation error fix

--- src/features/notFound/ui/views/not-found-view.tsx
+++ src/features/notFound/ui/views/not-found-view.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -11,7 +13,7 @@ export default function NotFoundView() {
       transition={{ duration: 0.6 }}
       className="flex flex-col md:flex-row items-center justify-center min-h-[70vh] px-6 md:px-12"
     >
-      <NotFoundImage/>
+      <NotFoundImage />
 
       <motion.div
         initial={{ x: 20, opacity: 0 }}
@@ -27,12 +29,15 @@ export default function NotFoundView() {
           </span>
         </p>
 
-        <Link href="/">
-          <Button className="inline-flex items-center bg-[#f47663] text-white font-semibold text-sm leading-none tracking-wide rounded-md px-[30px] py-[18px] shadow-md transition-all duration-300 transform hover:scale-[1.03] hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-opacity-50 hover:bg-[#9469c9]">
+        <Button
+          asChild
+          className="inline-flex items-center bg-[#f47663] text-white font-semibold text-sm leading-none tracking-wide rounded-md px-[30px] py-[18px] shadow-md transition-all duration-300 transform hover:scale-[1.03] hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-opacity-50 hover:bg-[#9469c9] cursor-pointer"
+        >
+          <Link href="/">
             <span>Back to Homepage</span>
-            <span className="ml-2 text-[9px] leading-none">→</span>
-          </Button>
-        </Link>
+            <span className="ml-2 text-[9px] leading-none ">→</span>
+          </Link>
+        </Button>
       </motion.div>
     </motion.main>
   );
